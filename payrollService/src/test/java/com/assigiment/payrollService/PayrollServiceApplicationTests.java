759a1d0becabeccad24e6ae9c15fa9e68e418ca3
Ammoniya <ravindushantha1995@gmail.com>
2020-07-18 20:03:41 +0530

    init

---
+++ payrollService/src/test/java/com/assigiment/payrollService/PayrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigiment.payrollService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
