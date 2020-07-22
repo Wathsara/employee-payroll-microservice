437a42493f35a5425baf11baa510f94876377825
Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>
2020-07-22 10:40:14 +0530

    Added cross origin

--- employeeDetailsService/src/main/java/com/assigiment/employeeDetailsService/EmployeeController.java
+++ employeeDetailsService/src/main/java/com/assigiment/employeeDetailsService/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class EmployeeController {
 
