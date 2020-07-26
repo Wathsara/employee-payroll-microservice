c2c67a242cc8ea02fde48332b40f54af9d1a467b
Ammoniya <ravindushantha1995@gmail.com>
2020-07-26 14:40:00 +0530

    setup PayrollRepository

--- payrollService/src/main/java/com/assigiment/payrollService/PayrollRepository.java
+++ payrollService/src/main/java/com/assigiment/payrollService/PayrollRepository.java
@@ -1,4 +1,6 @@
 package com.assigiment.payrollService;
 
-public interface PayrollRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayrollRepository extends JpaRepository<Payroll, Long> {
 }
