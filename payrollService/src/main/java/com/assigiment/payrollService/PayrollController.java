c4536c1c0cc01388c3597ed7920580d58be0fb28
Ammoniya <ravindushantha1995@gmail.com>
2020-07-26 14:56:03 +0530

    Payroll Controller Updated

--- payrollService/src/main/java/com/assigiment/payrollService/PayrollController.java
+++ payrollService/src/main/java/com/assigiment/payrollService/PayrollController.java
@@ -1,4 +1,27 @@
 package com.assigiment.payrollService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value = "/service1")
 public class PayrollController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to Software Engineering Assignment. PayrollService";
+    }
+
+    @RequestMapping(value = "/payroll", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE})
+    public String  getPayroll() {
+        return "Hello";
+    }
 }
